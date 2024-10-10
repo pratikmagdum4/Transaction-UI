65932d4950a99c6c2437f9689c9095ecba512d69
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-10-10 13:12:06 +0530

    minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./components/**/*.{js,jsx}",
     "./app/**/*.{js,jsx}",
     "./src/**/*.{js,jsx}",
+    
   ],
-
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
